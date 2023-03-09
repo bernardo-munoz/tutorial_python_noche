f3e222d7a4a56fc2c9bf960367874eb4eb30948a
berny1991 <bernardo0430@gmmail.com>
2023-03-08 19:53:06 -0500

    subiendo tipos de datos

---
+++ repasoPython/main.py
@@ -0,0 +1,29 @@
+# Tipos de datos
+# Numeros enteros
+
+numero1: int = 78
+numero2 = 45
+
+print(numero1)
+print(numero2)
+
+# Numeros reales
+
+numero3: float =4.9
+numero4 = 5.0
+
+print(numero3)
+print(numero4)
+
+#Booleano toman dos valores
+
+esColombia: bool = True
+esArgentino = False
+
+print(esColombia)
+print(esArgentino)
+
+# Caracter y cadena de caracteres
+
+mensaje = "Cadena con una comilla simple  /, una comilla doble, y una  diagonal invertida /"
+print(mensaje)
