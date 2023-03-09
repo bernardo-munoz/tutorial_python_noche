b1ffbe733243b650663f57ba7f9bad2f3ebf130e
berny1991 <bernardo0430@gmmail.com>
2023-03-08 20:32:36 -0500

    subiendo operadores

--- repasoPython/main.py
+++ repasoPython/main.py
@@ -27,3 +27,52 @@ print(esArgentino)
 
 mensaje = "Cadena con una comilla simple  /, una comilla doble, y una  diagonal invertida /"
 print(mensaje)
+
+#Operadores
+#Aritmaticos
+
+numero5 = 78
+numero6 = 46
+
+suma = numero5 + numero6
+resta = numero5 - numero6
+multiplicacion = numero5 * numero6
+division = numero5 / numero6
+modulo = numero5 % numero6
+
+print("la suma es", suma)
+print("la resta es", resta)
+print("la multiplicacion es", multiplicacion)
+print("la division es", division)
+print("el modulo es", modulo)
+
+#Asignacion
+
+x = 5
+y = 7
+z = 3
+
+#Logicos
+# and (y)
+
+p = 5
+q = 9
+print(p < 7 and q < 9)
+
+# or (o)
+print(p < 7 or q < 9)
+
+# not
+print(not(p < 7 or q < 7))
+
+# Relacionales
+
+edad1 = 23
+edad2 = 18
+print(edad1 == edad2 ) # igualdad
+print(edad1 > edad2 ) # Mayor que
+print(edad1 < edad2 ) # Menor que
+print(edad1 >= edad2 ) # Mayor o igual que
+print(edad1 <= edad2 ) # Menor o igual que
+print(edad1 != edad2 ) # No es igualdad
+
